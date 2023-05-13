2dfe991ab4ed084392a16893167c058d03f6ab2c
Hassan DRAGA <dragahassan@gmail.com>
2023-05-12 22:38:26 -0400

    Updating readme

---
+++ examples/minimal.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/webui-dev/go-webui"
+)
+
+func main() {
+
+	var my_window = webui.NewWindow()
+	webui.Show(my_window, "<html>Hello World</html>")
+	webui.Wait()
+}
