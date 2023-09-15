13fec90be7353dc6b84d8b57aac90ab404350b03
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-09-16 01:08:59 +0200

    Update `minimal` example to use window methods

--- examples/minimal.go
+++ examples/minimal.go
@@ -3,8 +3,8 @@ package main
 import "github.com/webui-dev/go-webui/v2"
 
 func main() {
-	var my_window = webui.NewWindow()
+	var w = webui.NewWindow()
 
-	webui.Show(my_window, "<html>Hello World</html>")
+	w.Show("<html>Hello World</html>")
 	webui.Wait()
 }
