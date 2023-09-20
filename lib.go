2470300defc514cee5e432e824fa41a675338021
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-09-21 01:57:42 +0200

    Fix doc comment for SetKiosk

--- lib.go
+++ lib.go
@@ -155,7 +155,7 @@ func (w Window) ShowBrowser(content string, browser Browser) (err error) {
 	return
 }
 
-// set_kiosk determines whether Kiosk mode (full screen) is enabled for the window.
+// SetKiosk determines whether Kiosk mode (full screen) is enabled for the window.
 func (w Window) SetKiosk(enable bool) {
 	C.webui_set_kiosk(C.size_t(w), C._Bool(enable))
 }
