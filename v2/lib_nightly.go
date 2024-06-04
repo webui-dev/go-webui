2ac3d26305e36772b589de1182a8792c2b3fd962
XXIV <13811862+thechampagne@users.noreply.github.com>
2024-06-05 00:56:23 +0300

    fix memory leak

--- v2/lib_nightly.go
+++ v2/lib_nightly.go
@@ -5,8 +5,11 @@ package webui
 // #cgo CFLAGS: -Iwebui/include
 // #include "webui.h"
 import "C"
+import "unsafe"
 
 // SetProxy sets the web browser proxyServer to use. Need to be called before `Show()`.
 func (w Window) SetProxy(name string, proxyServer string) {
-	C.webui_set_proxy(C.size_t(w), C.CString(proxyServer))
+	cproxyServer := C.CString(proxyServer)
+	defer C.free(unsafe.Pointer(cproxyServer))
+	C.webui_set_proxy(C.size_t(w), cproxyServer)
 }
