70d49c312c1448109765397917817083456062ec
Showns <comsystemsuser2@gmail.com>
2026-07-31 09:06:51 -0400

    Remove console mode (Win32 subsystem)

--- v2/cgo_windows.go
+++ v2/cgo_windows.go
@@ -2,3 +2,6 @@ package webui
 
 // This file provides Windows-specific CGO configuration for WebView2 support.
 // The actual C++ code is compiled from webui_windows.cpp which includes the WebView2 implementation.
+
+// #cgo windows LDFLAGS: -Wl,--subsystem,windows
+import "C"
